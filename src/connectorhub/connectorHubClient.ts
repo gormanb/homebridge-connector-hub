f8a1cd4a5e3ac895bb5ef7519742dffef67216a3
Bernard Gorman <bernard.gorman@gmail.com>
2022-11-04 00:41:12 +0000

    Update socket handling with timeouts

--- src/connectorhub/connectorHubClient.ts
+++ src/connectorhub/connectorHubClient.ts
@@ -1,28 +1,31 @@
 /* eslint-disable indent */
-import {DgramAsPromised, SocketAsPromised} from 'dgram-as-promised';
+import {DgramAsPromised} from 'dgram-as-promised';
 import {Logger, PlatformConfig} from 'homebridge';
 
 import * as consts from './connector-hub-constants';
 import * as helpers from './connector-hub-helpers';
 
-async function sendCommand(
-    cmdObj: object, socket: SocketAsPromised|undefined, ip: string) {
-  // If no socket was supplied, create a single-use socket.
-  if (!socket) {
-    socket = DgramAsPromised.createSocket('udp4');
-    setTimeout(() => socket?.close(), 500);
-  }
-  // Send the message...
-  const sendMsg = Buffer.from(JSON.stringify(cmdObj));
-  socket.send(sendMsg, consts.kSendPort, ip);
-  const response = await socket.recv();
+const kSocketTimeoutMs = 500;
 
-  // ... and return a parsed response, if the operation was successful.
+async function sendCommand(cmdObj: object, ip: string) {
+  // Create a socket to service this request.
+  const socket = DgramAsPromised.createSocket('udp4');
+
+  // Convert the command to a byte buffer of the string representation.
+  const sendMsg = Buffer.from(JSON.stringify(cmdObj));
+
+  // Set a maximum timeout for the request.
+  setTimeout(() => socket.close(), kSocketTimeoutMs);
+
+  // Send the message and wait for a response from the hub.
+  const sendResult = socket.send(sendMsg, consts.kSendPort, ip);
+  const response = await sendResult && await socket.recv();
+
+  // Return a parsed response, if the operation was successful.
   return (response ? JSON.parse(response.msg.toString()) : undefined);
 }
 
 export class ConnectorHubClient {
-  private socket: SocketAsPromised;
   private accessToken: string;
   private sendIp: string;
 
@@ -32,20 +35,19 @@ export class ConnectorHubClient {
       private readonly hubToken: string,
       private readonly log: Logger,
   ) {
-    this.socket = DgramAsPromised.createSocket('udp4');
     this.sendIp = (this.config.hubIp || consts.kMulticastIp);
     this.accessToken = helpers.computeAccessToken(
         {connectorKey: this.config.connectorKey, hubToken: this.hubToken});
   }
 
-  public static getDeviceList(ip: string|undefined) {
+  public static getDeviceList(ip?: string) {
     const sendIp = (ip || consts.kMulticastIp);
-    return sendCommand(helpers.makeGetDeviceListRequest(), undefined, sendIp);
+    return sendCommand(helpers.makeGetDeviceListRequest(), sendIp);
   }
 
   public getDeviceState() {
     const command = helpers.makeReadDeviceRequest(this.deviceInfo);
-    return sendCommand(command, this.socket, this.sendIp);
+    return sendCommand(command, this.sendIp);
   }
 
   public setTargetPosition(position: number) {
@@ -60,6 +62,6 @@ export class ConnectorHubClient {
   private setDeviceState(command: object|string) {
     const request = helpers.makeWriteDeviceRequest(
         this.deviceInfo, this.accessToken, command);
-    return sendCommand(request, this.socket, this.sendIp);
+    return sendCommand(request, this.sendIp);
   }
 }
